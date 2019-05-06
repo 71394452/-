3d9051382f0be39cf08fd9ea91e72d746a1a8e18
71394452 <47810028+71394452@users.noreply.github.com>
2019-05-06 18:54:13 +0800

    Update ssr.sh

--- ssr.sh
+++ ssr.sh
@@ -4,4 +4,4 @@ export PATH
 yum install unzip wget #For Centos
 apt install unzip wget #For Debian
 cd
-wget http://ssr.yh00.top/ssr-download/s123.zip && unzip ssr.zip && cd SSR* && bash install.sh
+wget http://ssr.yh00.top/ssr-download/ssr.zip && unzip ssr.zip && cd SSR* && bash install.sh
